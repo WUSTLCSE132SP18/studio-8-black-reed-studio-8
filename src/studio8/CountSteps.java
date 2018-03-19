5f8cbef3c883ad476face1f7a37b749348f09a64
tavis <tavis@LAPTOP-IC83BPPD.dhcp.wustl.edu>
2018-03-19 17:26:45 -0500

    Did some of it

--- src/studio8/CountSteps.java
+++ src/studio8/CountSteps.java
@@ -1,7 +1,35 @@
 package studio8;
+import java.io.IOException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Scanner;
+
 
 // TODO: Develop an algorithm to count steps in accelerometer data
 //    Major steeps:
 //       1. Create a class and main method.
 //       2. Using a Scanner and File object, read data from your .csv file.
 //       3. Develop and test algorithms to count the "peaks" in the data.
+public class CountSteps {
+	
+	public static void main(String[] args) throws IOException {
+		Path filepath= Paths.get("data/Stepsdata.csv");
+		Scanner scannedFile = new Scanner(filepath);
+		String [] x = new String[1000];
+		String [] y = new String[1000];
+		String [] z = new String[1000];
+
+		int i=0;
+		while (scannedFile.hasNext() && i<1000) {
+			String currentLine= scannedFile.nextLine();
+			String [] parts = currentLine.split(",");
+			x[i]=parts[0];
+			y[i]=parts[1];
+			z[i]=parts[2];
+			System.out.println(currentLine);
+			i++;
+		}
+		scannedFile.close();
+	}
+		
+}
---
+++ steps/steps.ino
@@ -0,0 +1,90 @@
+
+#include <Wire.h> // Must include Wire library for I2C
+#include <SparkFun_MMA8452Q.h> // Includes the SFE_MMA8452Q library
+
+// Begin using the library by creating an instance of the MMA8452Q
+//  class. We'll call it "accel". That's what we'll reference from
+//  here on out.
+MMA8452Q accel;
+
+// The setup function simply starts serial and initializes the
+//  accelerometer.
+void setup()
+{
+  Serial.begin(9600);
+  Serial.println("MMA8452Q Test Code!");
+  accel.init();
+
+}
+
+void loop()
+{
+  
+  if (accel.available())
+  {
+    // First, use accel.read() to read the new variables:
+    accel.read();
+    printCalculatedAccels();
+    //printOrientation();
+
+    Serial.println(); // Print new line every time.
+  }
+}
+
+// The function demonstrates how to use the accel.x, accel.y and
+//  accel.z variables.
+// Before using these variables you must call the accel.read()
+//  function!
+void printAccels()
+{
+  Serial.print(accel.x, 3);
+  Serial.print(",");
+  Serial.print(accel.y, 3);
+  Serial.print(",");
+  Serial.print(accel.z, 3);
+  Serial.print(",");
+}
+
+// This function demonstrates how to use the accel.cx, accel.cy,
+//  and accel.cz variables.
+// Before using these variables you must call the accel.read()
+//  function!
+void printCalculatedAccels()
+{
+  Serial.print(accel.cx, 3);
+  Serial.print(",");
+  Serial.print(accel.cy, 3);
+  Serial.print(",");
+  Serial.print(accel.cz, 3);
+  Serial.print(",");
+}
+
+// This function demonstrates how to use the accel.readPL()
+// function, which reads the portrait/landscape status of the
+// sensor.
+void printOrientation()
+{
+  // accel.readPL() will return a byte containing information
+  // about the orientation of the sensor. It will be either
+  // PORTRAIT_U, PORTRAIT_D, LANDSCAPE_R, LANDSCAPE_L, or
+  // LOCKOUT.
+  byte pl = accel.readPL();
+  switch (pl)
+  {
+    case PORTRAIT_U:
+      Serial.print("Portrait Up");
+      break;
+    case PORTRAIT_D:
+      Serial.print("Portrait Down");
+      break;
+    case LANDSCAPE_R:
+      Serial.print("Landscape Right");
+      break;
+    case LANDSCAPE_L:
+      Serial.print("Landscape Left");
+      break;
+    case LOCKOUT:
+      Serial.print("Flat");
+      break;
+  }
+}
